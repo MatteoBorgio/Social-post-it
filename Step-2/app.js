687b7fb722957b925e03075c89ecce4ad7eeb015
matte <matteo6.borgio@gmail.com>
2026-02-22 20:57:05 +0100

    add bootstrap style in gallery.ejs, post.ejs, register.ejs

--- Step-2/app.js
+++ Step-2/app.js
@@ -7,8 +7,6 @@ const fs = require('fs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, 'public')))
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
